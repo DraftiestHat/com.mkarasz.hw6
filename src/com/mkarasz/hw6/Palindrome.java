30b625128102944af986602fdb907eea2356bcff
DraftiestHat <mattchris9@hotmail.com>
2015-04-02 22:18:05 -0600

    finished frequency. no bug checking yet tho.

--- src/com/mkarasz/hw6/Palindrome.java
+++ src/com/mkarasz/hw6/Palindrome.java
@@ -1,6 +1,7 @@
 package com.mkarasz.hw6;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public final class Palindrome {
 	public static int shortestWordLength(ArrayList<String> list) {
@@ -75,7 +76,56 @@ public final class Palindrome {
 	}
 	
 	public static ArrayList<Double> letterFrequency(ArrayList<String> list) {
+		ArrayList<String> letterList = new ArrayList<String>();
+		ArrayList<Double> freqList = new ArrayList<Double>();
+		for(String i : list){
+			splitCharacters(i, letterList);
+		}
+		
+		freqList.add((double) Collections.frequency(list, "a"));
+		freqList.add((double) Collections.frequency(list, "b"));
+		freqList.add((double) Collections.frequency(list, "c"));
+		freqList.add((double) Collections.frequency(list, "d"));
+		freqList.add((double) Collections.frequency(list, "e"));
+		freqList.add((double) Collections.frequency(list, "f"));
+		freqList.add((double) Collections.frequency(list, "g"));
+		freqList.add((double) Collections.frequency(list, "h"));
+		freqList.add((double) Collections.frequency(list, "i"));
+		freqList.add((double) Collections.frequency(list, "j"));
+		freqList.add((double) Collections.frequency(list, "k"));
+		freqList.add((double) Collections.frequency(list, "l"));
+		freqList.add((double) Collections.frequency(list, "m"));
+		freqList.add((double) Collections.frequency(list, "n"));
+		freqList.add((double) Collections.frequency(list, "o"));
+		freqList.add((double) Collections.frequency(list, "p"));
+		freqList.add((double) Collections.frequency(list, "q"));
+		freqList.add((double) Collections.frequency(list, "r"));
+		freqList.add((double) Collections.frequency(list, "s"));
+		freqList.add((double) Collections.frequency(list, "t"));
+		freqList.add((double) Collections.frequency(list, "u"));
+		freqList.add((double) Collections.frequency(list, "v"));
+		freqList.add((double) Collections.frequency(list, "w"));
+		freqList.add((double) Collections.frequency(list, "x"));
+		freqList.add((double) Collections.frequency(list, "y"));
+		freqList.add((double) Collections.frequency(list, "z"));
+		
+		
+		
+		
+		
 		
 		return null;
+	}
+	
+	private static void splitCharacters(String i, ArrayList<String> list) {
+		int len = i.length();
+		String tmp = null;
+		
+		for(int j = 0; j < len; j++) {
+			System.out.println("j is " + j + " and the letter is " + i.substring(j, j+1));
+			tmp = i.substring(j, j + 1);
+			tmp.toLowerCase();
+			list.add(tmp);
+		}
 	}
 }
